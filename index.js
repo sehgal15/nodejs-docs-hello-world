65b3886c0c75ebbbefbc6f94d24406487a2b8c91
sehgal15 <88005442+sehgal15@users.noreply.github.com>
2022-11-28 11:09:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,10 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
